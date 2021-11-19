b7110a5ff60f1a7d394dbdee02311b9b079ca8b3
ZeeWanderer <mxx888777@gmail.com>
2021-11-19 09:10:33 -0800

    [Coding] add load_collection

--- Sources/Extension/Coding.swift
+++ Sources/Extension/Coding.swift
@@ -94,3 +94,54 @@ public extension JSONDecoder
         return dictionary
     }
 }
+
+//MARK: load_collection - json
+
+
+/// Loads json file from bundle and decodes it into collection
+/// - Parameters:
+///   - collection: inout collection to decode into
+///   - filename: name of the json file
+@inlinable
+public func load_collection<K, V, R>(_ collection: inout [K:V], _ filename: String)
+where K: RawRepresentable, K: Decodable, K.RawValue == R,
+      V: Decodable,
+      R: Decodable, R: Hashable
+{
+    let decoder = JSONDecoder()
+    if let path = Bundle.main.path(forResource: filename, ofType: "json")
+    {
+        do
+        {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+            collection = try decoder.decode([K:V].self, from: data as Data)
+        }
+        catch
+        {
+            print(error)
+        }
+    }
+}
+
+// TODO: investigate why this does not work with JSONDecoder::decode specialization for dicts
+/// Loads json file from bundle and decodes it into collection
+/// - Parameters:
+///   - collection: inout collection to decode into
+///   - filename: name of the json file
+@inlinable
+public func load_collection<T: Decodable>(_ collection: inout T, _ filename: String)
+{
+    let decoder = JSONDecoder()
+    if let path = Bundle.main.path(forResource: filename, ofType: "json")
+    {
+        do
+        {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+            collection = try decoder.decode(T.self, from: data as Data)
+        }
+        catch
+        {
+            print(error)
+        }
+    }
+}
