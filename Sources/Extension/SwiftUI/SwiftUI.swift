c4772ae1582fe5212e9ff987b32f5073af4920b2
ZeeWanderer <mxx888777@gmail.com>
2022-03-11 08:50:39 -0800

    [SwiftUI] EdgeInsets padding

--- Sources/Extension/SwiftUI/SwiftUI.swift
+++ Sources/Extension/SwiftUI/SwiftUI.swift
@@ -15,3 +15,30 @@ public extension UIEdgeInsets
         EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
     }
 }
+
+public extension EdgeInsets
+{
+    @inlinable
+    var paddingTrailing: CGFloat?
+    {
+        trailing != 0 ? trailing : nil
+    }
+    
+    @inlinable
+    var paddingLeading: CGFloat?
+    {
+        leading != 0 ? leading : nil
+    }
+    
+    @inlinable
+    var paddingTop: CGFloat?
+    {
+        top != 0 ? top : nil
+    }
+    
+    @inlinable
+    var paddingBottom: CGFloat?
+    {
+        bottom != 0 ? bottom : nil
+    }
+}
