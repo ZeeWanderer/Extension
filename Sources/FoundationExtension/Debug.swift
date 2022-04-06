1a4601a10249415d1e55dc3ae2b8aafce4848615
ZeeWanderer <mxx888777@gmail.com>
2022-04-06 03:27:57 -0700

    [Debug] use snake case for debug globals

--- Sources/FoundationExtension/Debug.swift
+++ Sources/FoundationExtension/Debug.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@available(*, deprecated, renamed: "debug_action(_:)")
 @inlinable
 public func debugAction(_ closure: () -> Void)
 {
@@ -16,7 +17,22 @@ public func debugAction(_ closure: () -> Void)
 }
 
 @inlinable
+public func debug_action(_ closure: () -> Void)
+{
+#if DEBUG
+    closure()
+#endif
+}
+
+@available(*, deprecated, renamed: "debug_print(_:separator:terminator:)")
+@inlinable
 public func debugPrint(_ items: Any..., separator: String = " ", terminator: String = "\n")
 {
-    debugAction { print(items, separator: separator, terminator: terminator) }
+    debug_action { print(items, separator: separator, terminator: terminator) }
+}
+
+@inlinable
+public func debug_print(_ items: Any..., separator: String = " ", terminator: String = "\n")
+{
+    debug_action { print(items, separator: separator, terminator: terminator) }
 }
--- Sources/UIKitExtension/Debug.swift
+++ Sources/UIKitExtension/Debug.swift
@@ -9,9 +9,25 @@ import UIKit
 import FoundationExtension
 
 /// Strokes specified rect in current CGContext if it can get one
+@available(*, deprecated, renamed: "debug_rect(_:color:)")
 public func debugRect(_ rect: CGRect, color: CGColor = UIColor.white.cgColor)
 {
-    debugAction {
+    debug_action {
+        if let ctx = UIGraphicsGetCurrentContext()
+        {
+            ctx.saveGState()
+            ctx.setStrokeColor(color)
+            let b_rect = UIBezierPath(rect: rect)
+            b_rect.stroke()
+            ctx.restoreGState()
+        }
+    }
+}
+
+/// Strokes specified rect in current CGContext if it can get one
+public func debug_rect(_ rect: CGRect, color: CGColor = UIColor.white.cgColor)
+{
+    debug_action {
         if let ctx = UIGraphicsGetCurrentContext()
         {
             ctx.saveGState()
