738979e3931e47674c610c5079f8e7a0e046891c
ZeeWanderer <mxx888777@gmail.com>
2022-05-23 06:56:09 -0700

    [Tests] add union tests

--- Tests/ExtensionTests/ExtensionTests.swift
+++ Tests/ExtensionTests/ExtensionTests.swift
@@ -9,12 +9,62 @@ import XCTest
 
 final class ExtensionTests: XCTestCase
 {
-    func testExample()
+    func testBoundingBoxValidity()
     {
-        debug_print("test")
+        let r1 = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        let r2 = CGRect(origin: CGPoint(x: 300, y: 300), size: CGSize(width: 100, height: 100))
+        let r3 = CGRect(origin: CGPoint(x: 1000, y: 200), size: CGSize(width: 100, height: 100))
+        let result1 = r1.union(r2)
+        let result2 = union([r1, r2])
+        
+        XCTAssertEqual(result1, result2)
+        
+        let result3 = result1.union(r3)
+        let result4 = union([r1, r2, r3])
+        
+        XCTAssertEqual(result3, result4)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    func testBoundingBox2Performance()
+    {
+        let r1 = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        let r2 = CGRect(origin: CGPoint(x: 300, y: 300), size: CGSize(width: 100, height: 100))
+        
+        measure {
+            let _ = r1.union(r2)
+        }
+    }
+    
+    func testBoundingBox3Performance()
+    {
+        let r1 = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        let r2 = CGRect(origin: CGPoint(x: 300, y: 300), size: CGSize(width: 100, height: 100))
+        let r3 = CGRect(origin: CGPoint(x: 1000, y: 200), size: CGSize(width: 100, height: 100))
+        
+        measure {
+            let result1 = r1.union(r2)
+            let _ = result1.union(r3)
+        }
+    }
+    
+    func testBoundingBoxArray2Performance()
+    {
+        let r1 = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        let r2 = CGRect(origin: CGPoint(x: 300, y: 300), size: CGSize(width: 100, height: 100))
+        
+        measure {
+            let _ = union([r1, r2])
+        }
+    }
+    
+    func testBoundingBoxArray3Performance()
+    {
+        let r1 = CGRect(origin: .zero, size: CGSize(width: 100, height: 100))
+        let r2 = CGRect(origin: CGPoint(x: 300, y: 300), size: CGSize(width: 100, height: 100))
+        let r3 = CGRect(origin: CGPoint(x: 1000, y: 200), size: CGSize(width: 100, height: 100))
+        
+        measure {
+            let _ = union([r1, r2, r3])
+        }
+    }
 }
