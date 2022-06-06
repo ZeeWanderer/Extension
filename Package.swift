653433b77d448c661adde22b2de7c3fd3dadf837
ZeeWanderer <mxx888777@gmail.com>
2022-06-06 07:17:38 -0700

    [Package] add swift-numerics[SE-0246]

--- Package.swift
+++ Package.swift
@@ -20,13 +20,16 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftExtension",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Numerics", package: "swift-numerics") // SE-0246 Accepted but not Implemented into Swift STD due to compiler issues
+            ]),
         .target(
             name: "FoundationExtension",
             dependencies: ["SwiftExtension"]),
