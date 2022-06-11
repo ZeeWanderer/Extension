2dcedcb9289c7163b4ddccdd4c07c9c101b8b4ca
ZeeWanderer <mxx888777@gmail.com>
2022-06-11 14:55:02 -0700

    [Concurrency] use new Task.sleep when compiling with swift 5.7+

--- Sources/FoundationExtension/Concurrency.swift
+++ Sources/FoundationExtension/Concurrency.swift
@@ -15,8 +15,20 @@ public extension Task where Success == Never, Failure == Never
     @inlinable
     static func sleep(seconds duration: TimeInterval) async throws
     {
+#if swift(>=5.7)
+        if #available(iOS 16, *)
+        {
+            try await Task.sleep(until: .now + .seconds(duration), clock: .continuous)
+        }
+        else
+        {
+            let delay = UInt64(duration * Double(NSEC_PER_SEC))
+            try await Task.sleep(nanoseconds: delay)
+        }
+#else
         let delay = UInt64(duration * Double(NSEC_PER_SEC))
         try await Task.sleep(nanoseconds: delay)
+#endif
     }
     
     /// Convenience function to delay execution in seconds. Intended for code clarity.
