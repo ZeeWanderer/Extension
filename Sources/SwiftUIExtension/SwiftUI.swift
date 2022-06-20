2676b3d2f7bc91f393c8c643770224487a2ad5db
ZeeWanderer <mxx888777@gmail.com>
2022-06-20 06:39:31 -0700

    [SwiftUI] hex initable Color

--- Sources/SwiftUIExtension/SwiftUI.swift
+++ Sources/SwiftUIExtension/SwiftUI.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// MARK: - UIEdgeInsets
 public extension UIEdgeInsets
 {
     @inlinable
@@ -16,6 +17,7 @@ public extension UIEdgeInsets
     }
 }
 
+// MARK: - EdgeInsets
 public extension EdgeInsets
 {
     /// Returns default padding if inset is 0
@@ -44,5 +46,18 @@ public extension EdgeInsets
     var bottomDefaultPadding: CGFloat?
     {
         bottom == 0 ? nil : 0
+    }
+}
+
+// MARK: - Color
+public extension Color
+{
+    @inlinable
+    init(_ colorSpace: Color.RGBColorSpace = .sRGB, hex: UInt32, opacity: CGFloat = 1)
+    {
+        let r = Double((hex & 0x00FF_0000) >> 16) / 255.0
+        let g = Double((hex & 0x0000_FF00) >> 8) / 255.0
+        let b = Double((hex & 0x0000_00FF) >> 0) / 255.0
+        self.init(colorSpace, red: r, green: g, blue: b, opacity: opacity)
     }
 }
