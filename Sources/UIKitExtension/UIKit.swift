e540fdedc6f593d5aab03b0481ff432fee7f9cba
ZeeWanderer <mxx888777@gmail.com>
2022-06-20 06:39:44 -0700

    [UIKit] hex initable UIColor

--- Sources/UIKitExtension/UIKit.swift
+++ Sources/UIKitExtension/UIKit.swift
@@ -7,6 +7,20 @@
 
 import UIKit
 
+// MARK: - UIColor
+public extension UIColor
+{
+    @inlinable
+    convenience init(hex: UInt32, alpha: CGFloat = 1)
+    {
+        let r = CGFloat((hex & 0x00FF_0000) >> 16) / 255.0
+        let g = CGFloat((hex & 0x0000_FF00) >> 8) / 255.0
+        let b = CGFloat((hex & 0x0000_00FF) >> 0) / 255.0
+        self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
+
+// MARK: - UIScreen
 public extension UIScreen
 {
     @MainActor
