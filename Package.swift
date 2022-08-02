ab8c3c437ee512f53efa2c32e2f0542bcaabcac0
ZeeWanderer <mxx888777@gmail.com>
2022-08-02 06:34:13 -0700

    [XCode] try fix SwiftUI previews on XCode 14 beta 4

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Extension",
-            type: .static,
+            //type: .static,
             targets: ["SwiftExtension", "FoundationExtension", "CoreGraphicsExtension", "UIKitExtension", "SpriteKitExtension", "SwiftUIExtension", "GeneralExtensions", "Extension"]),
     ],
     dependencies: [
