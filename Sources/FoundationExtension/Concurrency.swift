950f0e5e48b2accaaacd9861ed8bf5a83aaa63dd
ZeeWanderer <mxx888777@gmail.com>
2022-09-26 06:00:09 -0700

    [Foundation][Concurrency] Use Task.sleep(until:clock:) for iOS16+

--- Sources/FoundationExtension/Concurrency.swift
+++ Sources/FoundationExtension/Concurrency.swift
@@ -11,23 +11,20 @@ import Foundation
 public extension Task where Success == Never, Failure == Never
 {
     /// Convenience function to sleep in seconds.
-    /// - Note: Function converts `TimeInterval` to nanoseconds so mind the upper `Double` limit of `10^308.3`.
+    /// - Note: On iOS15 and lower function converts `TimeInterval` to nanoseconds so mind the upper `Double` limit of `10^308.3`. On iOS16+ `Instant` API i used.
     @inlinable
     static func sleep(seconds duration: TimeInterval) async throws
     {
 
-//        if #available(iOS 16, *)
-//        {
-//            try await Task.sleep(until: .now + .seconds(duration), clock: .continuous)
-//        }
-//        else
-//        {
-//            let delay = UInt64(duration * Double(NSEC_PER_SEC))
-//            try await Task.sleep(nanoseconds: delay)
-//        }
-        
-        let delay = UInt64(duration * Double(NSEC_PER_SEC))
-        try await Task.sleep(nanoseconds: delay)
+        if #available(iOS 16, *)
+        {
+            try await Task.sleep(until: .now + .seconds(duration), clock: .continuous)
+        }
+        else
+        {
+            let delay = UInt64(duration * Double(NSEC_PER_SEC))
+            try await Task.sleep(nanoseconds: delay)
+        }
     }
     
     /// Convenience function to delay execution in seconds. Intended for code clarity.
