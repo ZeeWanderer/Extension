7d1b28fb4f24e11add4d4c760424e8b4f49707eb
ZeeWanderer <mxx888777@gmail.com>
2024-08-02 18:34:11 +0300

    Create build_docs.sh

---
+++ build_docs.sh
@@ -0,0 +1,3 @@
+xcodebuild docbuild -scheme Extension \
+    -destination generic/platform=iOS \
+    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path Extension --output-path docs"
