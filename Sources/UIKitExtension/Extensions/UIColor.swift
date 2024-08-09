712f9bf5d41d0ea06d4ced084b154af8453dbf07
ZeeWanderer <mxx888777@gmail.com>
2024-08-10 01:06:39 +0300

    [UIKit][UIColor] init?(hex: String)

--- Sources/UIKitExtension/Extensions/UIColor.swift
+++ Sources/UIKitExtension/Extensions/UIColor.swift
@@ -9,12 +9,85 @@ import UIKit
 
 public extension UIColor
 {
+    /// - Parameter hex: a hex literal color representation, 3 bytes sans alpha
+    /// - Parameter alpha: color alpha
     @inlinable
     convenience init(hex: UInt32, alpha: CGFloat = 1)
     {
-        let r = CGFloat((hex & 0x00FF_0000) >> 16) / 255.0
-        let g = CGFloat((hex & 0x0000_FF00) >> 8) / 255.0
-        let b = CGFloat((hex & 0x0000_00FF) >> 0) / 255.0
+        let r = CGFloat((hex & 0x00_FF_00_00) >> 16) / 255.0
+        let g = CGFloat((hex & 0x00_00_FF_00) >> 8) / 255.0
+        let b = CGFloat((hex & 0x00_00_00_FF) >> 0) / 255.0
         self.init(red: r, green: g, blue: b, alpha: alpha)
+    }
+    
+    /// - Parameter hex: a hex literal color representation, 4 bytes
+    /// in standard # format or any recognizable hex format
+    @inlinable
+    convenience init?(hex: String)
+    {
+        let r, g, b, a: CGFloat
+        
+        let offsetBy = hex.hasPrefix("#") ? 1 : (hex.hasPrefix("0x") || hex.hasPrefix("0X") ? 2 : 0)
+        let start = hex.index(hex.startIndex, offsetBy: offsetBy)
+        let hexColor = String(hex[start...])
+        
+        if hexColor.count == 8
+        {
+            let scanner = Scanner(string: hexColor)
+            var hexNumber: UInt64 = 0
+            
+            if scanner.scanHexInt64(&hexNumber)
+            {
+                r = CGFloat((hexNumber & 0xFF_00_00_00) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00_FF_00_00) >> 16) / 255
+                b = CGFloat((hexNumber & 0x00_00_FF_00) >> 8) / 255
+                a = CGFloat((hexNumber & 0x00_00_00_FF) >> 0) / 255
+                
+                self.init(red: r, green: g, blue: b, alpha: a)
+                return
+            }
+        }
+        
+        return nil
+    }
+    
+    /// - Parameter hex: a hex literal color representation, 3 or 4 bytes
+    /// in standard # format or any recognizable hex format.
+    /// - Parameter alpha: color alpha
+    /// - Important: If 4 bytes are provided, `alpha` parameter overrides alpha from `hex`
+    @inlinable
+    convenience init?(hex: String, alpha: CGFloat)
+    {
+        let r, g, b: CGFloat
+        
+        let offsetBy = hex.hasPrefix("#") ? 1 : (hex.hasPrefix("0x") || hex.hasPrefix("0X") ? 2 : 0)
+        let start = hex.index(hex.startIndex, offsetBy: offsetBy)
+        let hexColor = String(hex[start...])
+        
+        let scanner = Scanner(string: hexColor)
+        var hexNumber: UInt64 = 0
+        
+        guard scanner.scanHexInt64(&hexNumber) else { return nil }
+        
+        if hexColor.count == 6
+        {
+            r = CGFloat((hexNumber & 0x00_FF_00_00) >> 16) / 255.0
+            g = CGFloat((hexNumber & 0x00_00_FF_00) >> 8) / 255.0
+            b = CGFloat((hexNumber & 0x00_00_00_FF) >> 0) / 255.0
+            
+            self.init(red: r, green: g, blue: b, alpha: alpha)
+            return
+        }
+        else if hexColor.count == 8
+        {
+            r = CGFloat((hexNumber & 0xFF_00_00_00) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00_FF_00_00) >> 16) / 255
+            b = CGFloat((hexNumber & 0x00_00_FF_00) >> 8) / 255
+            
+            self.init(red: r, green: g, blue: b, alpha: alpha)
+            return
+        }
+        
+        return nil
     }
 }
--- Tests/ExtensionTests/ExtensionTests.swift
+++ Tests/ExtensionTests/ExtensionTests.swift
@@ -79,4 +79,22 @@ final class ExtensionTests: XCTestCase
         XCTAssertEqual(CGSize(side: 75), r3)
         XCTAssertEqual(r4, r3)
     }
+    
+    func testHEXStringToColor()
+    {
+        let r0 = UIColor(hex: 0xFF00FF, alpha: 0)
+        let r1 = UIColor(hex: "FF00FF00")
+        let r2 = UIColor(hex: "FF00FF", alpha: 0)
+        let r3 = UIColor(hex: "#FF00FF00")
+        let r4 = UIColor(hex: "0xFF00FF00")
+        let r5 = UIColor(hex: "0XFF00FF00")
+        let r6 = UIColor(hex: "0XFF00FFFF", alpha: 0)
+        
+        XCTAssertEqual(r0, r1)
+        XCTAssertEqual(r0, r2)
+        XCTAssertEqual(r0, r3)
+        XCTAssertEqual(r0, r4)
+        XCTAssertEqual(r0, r5)
+        XCTAssertEqual(r0, r6)
+    }
 }
