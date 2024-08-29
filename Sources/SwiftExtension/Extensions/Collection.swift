b8c95a1d55665e0b4979d10b142fea1cf5da76ac
ZeeWanderer <mxx888777@gmail.com>
2024-08-29 13:37:06 +0300

    [Swift][Collection] isNotEmpty

---
+++ Sources/SwiftExtension/Extensions/Collection.swift
@@ -0,0 +1,15 @@
+//
+//  Collection.swift
+//
+//
+//  Created by zee wanderer on 29.08.2024.
+//
+
+import Foundation
+
+public extension Collection
+{
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
