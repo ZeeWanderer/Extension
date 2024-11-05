1c47ccf3efaaea4360b165012776a42c2357ecd2
ZeeWanderer <mxx888777@gmail.com>
2024-11-05 17:47:44 +0200

    [CI] try fix docbuild

--- build_docs.sh
+++ build_docs.sh
@@ -1,3 +1,3 @@
-xcodebuild docbuild -scheme Extension \
+xcodebuild docbuild -skipMacroValidation -scheme Extension \
     -destination generic/platform=iOS \
     OTHER_DOCC_FLAGS='--transform-for-static-hosting --hosting-base-path Extension --output-path docs'
