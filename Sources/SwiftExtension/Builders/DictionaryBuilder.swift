2b633c4f1943c33e751ebdf2f890a3d3a3c56312
ZeeWanderer <kulik.maksim.sergeevich@gmail.com>
2025-03-13 20:35:54 +0200

    [Swift] DictionaryBuilder visibility fix

--- Sources/SwiftExtension/Builders/DictionaryBuilder.swift
+++ Sources/SwiftExtension/Builders/DictionaryBuilder.swift
@@ -6,31 +6,31 @@
 //
 
 @resultBuilder
-struct DictionaryBuilder<Key: Hashable, Value>
+public struct DictionaryBuilder<Key: Hashable, Value>
 {
-    static func buildBlock(_ components: [Key: Value]...) -> [Key: Value]
+    public static func buildBlock(_ components: [Key: Value]...) -> [Key: Value]
     {
         components.reduce(into: [:]) { result, component in
             result.merge(component) { (_, new) in new }
         }
     }
     
-    static func buildOptional(_ component: [Key: Value]?) -> [Key: Value]
+    public static func buildOptional(_ component: [Key: Value]?) -> [Key: Value]
     {
         component ?? [:]
     }
     
-    static func buildEither(first component: [Key: Value]) -> [Key: Value]
+    public static func buildEither(first component: [Key: Value]) -> [Key: Value]
     {
         component
     }
     
-    static func buildEither(second component: [Key: Value]) -> [Key: Value]
+    public static func buildEither(second component: [Key: Value]) -> [Key: Value]
     {
         component
     }
     
-    static func buildArray(_ components: [[Key: Value]]) -> [Key: Value]
+    public static func buildArray(_ components: [[Key: Value]]) -> [Key: Value]
     {
         components.reduce(into: [:]) { result, component in
             result.merge(component) { (_, new) in new }
@@ -38,7 +38,7 @@ struct DictionaryBuilder<Key: Hashable, Value>
     }
 }
 
-func makeDictionary<Key: Hashable, Value>(@DictionaryBuilder<Key, Value> _ content: () -> [Key: Value]) -> [Key: Value]
+public func makeDictionary<Key: Hashable, Value>(@DictionaryBuilder<Key, Value> _ content: () -> [Key: Value]) -> [Key: Value]
 {
     content()
 }
